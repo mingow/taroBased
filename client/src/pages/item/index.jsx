2fb43a9bf014c9e6306c7580ea07b8d7297f90d0
mingo <mingo@mingodeMac-mini.lan>
2019-11-25 03:00:50 +0800

    add detail

--- client/src/pages/item/index.jsx
+++ client/src/pages/item/index.jsx
@@ -10,7 +10,7 @@ export default class Index extends Component {
     navigationBarTitleText: '详情',
     backgroundColor:'#ffffff',
     backgroundColorTop:'#ffffff',
-    backgroundColorBottom:'#ffffff'
+    backgroundColorBottom:'#eee'
   }
 
   constructor (props) {
@@ -33,8 +33,14 @@ export default class Index extends Component {
           '【五黑房】超强吃机主机配34寸曲面显示器，祝你大吉大利',
           '【KTV】20平米海量曲库超大练歌房，让你想唱就唱',
           '【电影房】高清投影巨幅荧幕，让你身临其境',
-          '【桌游】玉女烈白混，谁水谁先die'
-        ]
+          '【桌游】玉女烈白混，谁水谁先die',
+          '【说明】',
+          '本次费用包含30人套餐价格，超出人员按68元/人收取费用，场地布置有28人次床铺，超出此人数将无法安排床位，我们的管家会尽可能为您协调，还请谅解。',
+          '场地提供自助厨房，如需动火做饭需额外加收200元清洁费。',
+          '场地提供各类酒水，均按照超市价格出售，可提前冰镇，欢迎和管家咨询。',
+          '由于有多个预定渠道，可能出现预定失败的情况，预定失败支付的定金会原路退回，还请谅解（如非顾客原因导致预定失败，将赠送一张优惠券，我们将竭尽所能满足顾客的需求）',
+        ],
+        more:'https://mp.weixin.qq.com/s/MYfcxgegSKjch2DMBcBZbw'
       },
       static:{
         session:{
@@ -75,6 +81,17 @@ export default class Index extends Component {
   changeTag(e) {
     this.setState({session:e.name},this.pushInfo);
 
+  }
+
+  more() {
+    console.log('success')
+    Taro.navigateTo({
+      url:'/pages/webview/index?src='+this.state.data.more,
+      success: function(res) {
+        // 通过eventChannel向被打开页面传送数据
+        res.eventChannel.emit('sendData', {})
+      }
+    })
   }
 
   checkAvailable(){
@@ -120,7 +137,7 @@ export default class Index extends Component {
 
     }
     wx.setNavigationBarTitle({
-      title: 'Homie欢乐轰趴·龙岸花园店'
+      title: this.state.data.name
     })
   }
 
@@ -129,58 +146,61 @@ export default class Index extends Component {
       return <View key={index} className='at-article__p article_p'>{i}</View>
     })
     return (
-      <View className='index'>
-        <AtMessage />
-        <Swiper
-          className='test-h'
-          style={{height:this.state.width}}
-          indicatorColor='#999'
-          indicatorActiveColor='#333'
-          circular
-          autoplay>
-          <SwiperItem>
-            <CloudImage cloudId='cloud://vue-homeparty-4iqxy.7675-vue-homeparty-4iqxy-1300407309/resources/images/photos/0.JPG' ></CloudImage>
-          </SwiperItem>
-          <SwiperItem>
-            <CloudImage cloudId='cloud://vue-homeparty-4iqxy.7675-vue-homeparty-4iqxy-1300407309/resources/images/banners/sales00.jpg' ></CloudImage>
-          </SwiperItem>
-          <SwiperItem>
-            <CloudImage cloudId='cloud://vue-homeparty-4iqxy.7675-vue-homeparty-4iqxy-1300407309/resources/images/banners/sales00.jpg' ></CloudImage>
-          </SwiperItem>
-        </Swiper>
-        <View className='header'>
-          <Text className='primary'>{this.state.data.name}</Text>
-          <Text className='discription'>{this.state.data.desciption}</Text>
-          <Text>￥{this.state.data.price}元起</Text>
-          <Text>￥{this.state.data.oriPrice}元起</Text>
-        </View>
-        <View className='contents'>
-          {content}
-        </View>
-        <View>
-          <WebView src = 'https://mp.weixin.qq.com/s/MYfcxgegSKjch2DMBcBZbw' binderror = {this.error.bind(this)}></WebView>
-        </View>
-        {/* 以下部分是底部菜单栏 */}
-        <View className='bottom at-row safeArea'>
-          <View className='grid left at-col at-col-6 at-row'>
-            <View className='at-col iButton'>
-              <View><AtIcon prefixClass='icon' value='home' size='24' color='#666'></AtIcon></View>
-              <Text>首页</Text>
+      <View>
+        <View className='index safeAreaM'>
+          <AtMessage />
+          <Swiper
+            className='test-h'
+            style={{height:this.state.width}}
+            indicatorColor='#999'
+            indicatorActiveColor='#333'
+            circular
+            autoplay>
+            <SwiperItem>
+              <CloudImage cloudId='cloud://vue-homeparty-4iqxy.7675-vue-homeparty-4iqxy-1300407309/resources/images/photos/0.JPG' ></CloudImage>
+            </SwiperItem>
+            <SwiperItem>
+              <CloudImage cloudId='cloud://vue-homeparty-4iqxy.7675-vue-homeparty-4iqxy-1300407309/resources/images/banners/sales00.jpg' ></CloudImage>
+            </SwiperItem>
+            <SwiperItem>
+              <CloudImage cloudId='cloud://vue-homeparty-4iqxy.7675-vue-homeparty-4iqxy-1300407309/resources/images/banners/sales00.jpg' ></CloudImage>
+            </SwiperItem>
+          </Swiper>
+          <View className='header'>
+            <Text className='primary'>{this.state.data.name}</Text>
+            <Text className='discription'>{this.state.data.desciption}</Text>
+            <Text>￥{this.state.data.price}元起</Text>
+            <Text>￥{this.state.data.oriPrice}元起</Text>
+          </View>
+          <View className='contents'>
+            {content}
+            <Text onClick={this.more.bind(this)}>查看更多</Text>
+          </View>
+          <View className='safeArea'></View>
+
+          {/* 以下部分是底部菜单栏 */}
+          <View className='bottom at-row safeArea'>
+            <View className='grid left at-col at-col-6 at-row'>
+              <View className='at-col iButton'>
+                <View><AtIcon prefixClass='icon' value='home' size='24' color='#666'></AtIcon></View>
+                <Text>首页</Text>
+              </View>
+              <View className='at-col iButton'>
+                <View><AtIcon prefixClass='icon' value='dizhi' size='24' color='#666'></AtIcon></View>
+                <Text>位置</Text>
+              </View>
+              <View className='at-col iButton'>
+                <View><AtIcon prefixClass='icon' value='tel-fill' size='24' color='#666'></AtIcon></View>
+                <Text>客服</Text>
+              </View>
             </View>
-            <View className='at-col iButton'>
-              <View><AtIcon prefixClass='icon' value='dizhi' size='24' color='#666'></AtIcon></View>
-              <Text>位置</Text>
-            </View>
-            <View className='at-col iButton'>
-              <View><AtIcon prefixClass='icon' value='tel-fill' size='24' color='#666'></AtIcon></View>
-              <Text>客服</Text>
+            <View  className='safeArea grid at-col right at-col-6'>
+              <Text onClick={this.openFloatLayer.bind(this)} >立即预定</Text>
             </View>
           </View>
-          <View  className='safeArea grid at-col right at-col-6'>
-            <Text onClick={this.openFloatLayer.bind(this)} >立即预定</Text>
-          </View>
+          {/* 以下部分是弹出层内容 */}
+
         </View>
-        {/* 以下部分是弹出层内容 */}
         <AtFloatLayout className='layer' isOpened={this.state.buy} onClose={this.closeFloatLayer.bind(this)}>
           <View className='at-row'>
             <View className='at-col--auto img'>
